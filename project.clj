eafaa0f3a8ccd1690e1f686a2747af0a111341b7
Twan Coenraad <tcoenraad@users.noreply.github.com>
2021-11-25 15:17:51 +0100

    v1.2.1-alpha2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
-(defproject com.nedap.staffing-solutions/utils.spec.predicates "1.2.1-alpha1"
+(defproject com.nedap.staffing-solutions/utils.spec.predicates "1.2.1-alpha2"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[com.nedap.staffing-solutions/speced.def "1.1.1"]
                  [org.clojure/clojure "1.10.1"]]
